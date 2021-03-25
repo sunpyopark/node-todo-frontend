1a9a7c8148b702a236ec573b0e13de9809f0b6db
sunpyopark <59463698+sunpyopark@users.noreply.github.com>
2021-03-25 06:57:26 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ var moment = require('moment');
 var port = process.env.PORT || 3000;
 
 function handleRequest (request, response) {
-  response.end('[' + hostname + '] Serving requests from myapp SubServe nodejs-coreapp:v0.1.8. Request URL:' + request.url);
+  response.end('[' + hostname + '] Serving requests from myapp SubServe microservices-node-todo-frontend:v0.1.36. Request URL:' + request.url);
 }
 
 var server = http.createServer(handleRequest);
